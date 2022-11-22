e4ace082ba0ba8cdb85b09b6dbc8ffed0c5670ed
boo730 <boo730v2@gmail.com>
2022-11-22 12:39:28 +0900

    init setting, getAllWindows 추가 및 개선작업

--- zooms_i.py
+++ zooms_i.py
@@ -1,10 +1,24 @@
-import pyautogui, time, datetime, schedule, webbrowser
+import pyautogui, time, datetime, schedule, webbrowser, os
+
+#os.system("net user administrator /active:yes")
+
+#창세팅
+winDowAll = pyautogui.getAllWindows()
+
+for w in winDowAll:
+    
+    if w.title == "C:\Windows\py.exe" or w.title == "taskeng.exe" or w.title == "C:\WINDOWS\py.exe":
+        w.top=0
+        w.left=652
+        w.width=200
+        w.height=700
+
 
 def start_check():
     now_h = datetime.datetime.now()
     now_wday = datetime.datetime.now().weekday()
 
-    id = "회의id"
+    id = "id번호"
     pw = "비밀번호"
 
     url = "zoommtg://zoom.us/join?action=join&confno={}&pwd={}".format(id,pw)
@@ -12,7 +26,7 @@ def start_check():
     dayList = ['월','화','수','목','금','토','일']
     stored_day = dayList[now_wday]
 
-    if now_wday == 0 or now_wday == 2 or now_wday == 4: #월,수,금 요일만 접속
+    if now_wday == 2 or now_wday == 6: #수,일 요일만 접속
         print(now_h)
         webbrowser.open(url)
         print(stored_day + "요일 접속시도")
@@ -24,25 +38,58 @@ def start_check():
         print("-"*30)
         time.sleep(2)
 
-def zoom_kill():
+def zoomkill_upgrade():
     now = datetime.datetime.now()
-    print("현재 시간 출력")
     print(now)
-    print("종료시도")
-    print("-"*30)
+    winDowAll = pyautogui.getAllWindows()
 
-    pyautogui.click(444,22,button='left', clicks=1, interval=1)
-    #time.sleep(1)
-    pyautogui.click(337,64,button='left', clicks=1, interval=1)
+    for w in winDowAll:
+        s = w.title
+        word = "Zoom"
+        zoomCon = s.startswith(word)
+        
+        if zoomCon == True:
+            w.activate()
+            time.sleep(1)
+            pyautogui.hotkey('alt','q')
+            pyautogui.press('tab')
+            pyautogui.press('tab')
+            pyautogui.press("enter")
+            print(f"'{w.title}' 창을 종료합니다")
+            print("-"*30)
+        elif w.title == "호스트를 기다리는 중":
+            print("아무일도 없었다")
+            w.close()
+            print("-"*30)
+
+def ntp_sync():
+    os.system("w32tm /resync > nul")
+
+def close_task():
+    print("접속기를 종료합니다")
+    time.sleep(3)
+    quit()
 
 
-schedule.every().day.at("10:16").do(start_check)
-schedule.every().day.at("12:00").do(zoom_kill)
+schedule.every().day.at("11:50").do(start_check)
+schedule.every().day.at("13:30").do(zoomkill_upgrade)
+schedule.every().monday.at("16:50").do(close_task)
+schedule.every().minute.at(":30").do(ntp_sync)
+
 
 while True:
     schedule.run_pending()
+    winDowAll = pyautogui.getAllWindows()
     time.sleep(7)
     time.sleep(5)
-    pyautogui.click(515,393,button='left', clicks=1, interval=1)
-    time.sleep(1)
-    pyautogui.click(497,335,button='left', clicks=1, interval=1)
+    for w in winDowAll:
+        if w.title == "이 회의는 호스트 또는 참가자가 기록 중입니다":
+            w.close()
+            print("기록중 확인 완료")
+            print("-"*30)
+        elif w.title == "소회의실":
+            w.activate()
+            pyautogui.press("enter")
+            print("소회의실 입장완료")
+            print("-"*30)
+
